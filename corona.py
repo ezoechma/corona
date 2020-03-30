775a79823170938efb5f4fca3ab95bfa78b1641e
Erich <ezoechma@nt.tuwien.ac.at>
2020-03-30 09:55:08 +0200

    small fixes and added figures

--- corona.py
+++ corona.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 import pandas as pd
-import requests
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.pyplot import cm
 from scipy.optimize import curve_fit
+from scipy.signal import savgol_filter
 import seaborn as sns
 sns.set()
 sns.set_context("notebook",font_scale=1.8)
@@ -23,26 +23,26 @@ def fit_expo(x,y):
     return params[0],params[1],initParams
 
 
-#now         = datetime.now() 
-#date        = now.strftime("%Y-%m-%d") 
-
-date        = '2020-03-21'  #hard coded
-
+now         = datetime.now() 
+date        = now.strftime("%Y-%m-%d") 
+date        = '2020-03-29'  #hard coded
 url         = "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-"
 url         = url +date+".xlsx"
 
 df          = pd.read_excel(url)
-#df.head() #show table
-
 country     = 'Italy' #e.g. South Korea,China, United_States_of_America,United_Kingdom, Italy, Austria
-df_filtered = df[df['Countries and territories'] == country]  
+df_filtered = df[df['countriesAndTerritories'] == country]  
+#until 2020-03-26  'Countries and territories'
 
 #df_filtered['Cases'].describe()  # describtive statistics
 
-timeSpan    = 31  # last month
-dates       = np.array(df_filtered['DateRep'][0:timeSpan],dtype='datetime64[D]')
+timeSpan    = 7*5  # last month
+dates       = np.array(df_filtered['dateRep'][0:timeSpan],dtype='datetime64[D]')
+#until 2020-03-26   'DateRep'
+
 dates       = dates[::-1]
-newCases    = np.array(df_filtered['Cases'])[0:timeSpan] 
+newCases    = np.array(df_filtered['cases'])[0:timeSpan] 
+#unitl 2020-03-26    'Cases'                       
 newCases    = newCases[::-1]  #reverse order 
 allCases    = np.cumsum(newCases)
 xTicks      = np.arange(np.size(newCases))
@@ -53,7 +53,7 @@ params,_, _ = fit_expo(xTicks,allCases)
 plt.plot(xTicks, params[0]*np.exp(params[1]*xTicks),'r--', linewidth = 4, label = 'whole time span')
 
 ######## sliding fit ###########
-fittedDays   = 7
+fittedDays   = 5
 n            = timeSpan-fittedDays+1  #samples for sliding fit
 
 colorMap     = cm.rainbow(np.linspace(1,0,n)) #e.g. rainbow, magma
@@ -86,4 +86,27 @@ plt.xticks(xTicks[fittedDays-1::3], dates[fittedDays-1::3], rotation=30)
 plt.ylabel('Doubling Intervals in days', fontsize = 22)
 plt.title(country.replace('_',' '), fontsize = 26)
 plt.grid(True)
+plt.ylim([0, doublingVec[-1]*2])
 plt.show()
+
+plt.figure(3)
+relGrowth =100*(allCases[1::]-allCases[:-1:])/allCases[:-1:]
+plt.bar(xTicks[1::],relGrowth,color = 'k', alpha=1  , align = 'center')   
+plt.title(country.replace('_',' '), fontsize = 26)
+plt.xticks(xTicks[1::3], dates[1::3], rotation=30)
+target = 10
+plt.axhline(target, 0, 1,c='r', label='Austrian target')
+plt.ylim([0, 100])
+plt.ylabel('relative growth in %', fontsize = 22)
+plt.legend(loc=1)
+plt.show()
+
+plt.figure(4)
+xVal = savgol_filter(allCases, 5, 3)   #filter length, filter order
+yVal = savgol_filter(newCases, 5, 3)
+plt.loglog(xVal,yVal)
+plt.xlim((100,max(xVal)*1.1))
+plt.ylim((10,max(xVal)*1.1))
+plt.xlabel('all cases')
+plt.ylabel('new cases')
+plt.grid(True)
